19b062456e501e75700ae6501f558c8e1b220765
Suand9704 <sunandvemavarapu@gmail.com>
2025-07-06 08:22:11 +0530

    googlelogin

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,6 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "dxwcflceu",
-  api_key: process.env.CLOUDINARY_API_KEY || "784244663461292",
-  api_secret: process.env.CLOUDINARY_API_SECRET || "Xzrx-hgcxnzTnL8qaGBbisGIYHE",
 });
 
 module.exports = cloudinary;
