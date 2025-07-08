917388489017498cf323a389983a4dd953e7613c
Hosanna-Mosa <Hosannamosa4190@gmail.com>
2025-07-08 22:45:51 +0530

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,7 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME ||"dk6rrrwum",
-  api_key: process.env.CLOUDINARY_API_KEY || "862928933218424",
-  api_secret: process.env.CLOUDINARY_API_SECRET  || "d8SVdViROd7pf6mgRgS2ndZbLwM",
+  
 });
 
 module.exports = cloudinary;
