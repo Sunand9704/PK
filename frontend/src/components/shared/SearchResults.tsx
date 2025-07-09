978d8362ff96412459af43b00319191da4bbb0d8
Sunand-Vemavarapu <sunandvemavarapu@gmail.com>
2025-07-09 10:37:32 +0530

    Update SearchResults.tsx

--- frontend/src/components/shared/SearchResults.tsx
+++ frontend/src/components/shared/SearchResults.tsx
@@ -130,8 +130,7 @@ export const SearchResults: React.FC<SearchResultsProps> = ({
           </div>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {products.map((product) => (
-                      ))}
+            
           </div>
         )}
       </div>
